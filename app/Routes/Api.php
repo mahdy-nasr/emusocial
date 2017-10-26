f492112960a8edf416c2b0d6d3c23ed59ed3b5ee
mahdy-nasr <mahdy.m.nasr@gmail.com>
2017-10-26 12:10:54 +0300

    test dummy api

--- app/Routes/Api.php
+++ app/Routes/Api.php
@@ -1,1 +1,6 @@
 <?php
+$app->get('/api/students', function ( $request,  $response,$arguments) {
+	$arr = ['RC' => 200,'results' => ['id'=>1,'username'=>'elmahdy sharidy','student_number'=>'146932']];
+	return $response->withJson($arr,200);
+
+});
