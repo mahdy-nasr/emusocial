4822a31719d590309fd6a6929c24aeb037a98f64
mahdy-nasr <mahdy.m.nasr@gmail.com>
2018-01-20 21:05:42 +0200

    post link

--- app/Models/User.php
+++ app/Models/User.php
@@ -204,7 +204,7 @@ class User extends \App\Base
 
     public function addNotificationToken($token)
     {
-        $this->db->write("UPDATE user set notification_token = '' where notification_token = '{$token}'");
+        $this->db->write("UPDATE user set notification_token = null where notification_token = '{$token}'");
         return $this->db->write("UPDATE user set notification_token = ? where id = {$this->getId()}",[$token]);
     }
 
